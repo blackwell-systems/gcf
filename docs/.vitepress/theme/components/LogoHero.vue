734a911fe1556ab3719bd6f68f42069dc9ed17bc
Dayna Blackwell <dayna@blackwell-systems.com>
2026-06-14 20:16:39 -0700

    experiment: replace hero name with logo wordmark

---
+++ docs/.vitepress/theme/components/LogoHero.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="logo-hero">
+    <img src="/gcf-logo.png" alt="GCF" class="logo-img" />
+  </div>
+</template>
+
+<style scoped>
+.logo-hero {
+  text-align: center;
+  padding: 0 24px 8px;
+}
+
+.logo-img {
+  max-width: 200px;
+  height: auto;
+}
+</style>
