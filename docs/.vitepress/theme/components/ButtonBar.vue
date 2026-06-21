9d1d94ec2a4c2026fdf719f1379ae1c2e1a82b29
Dayna Blackwell <dayna@blackwell-systems.com>
2026-06-20 18:22:16 -0700

    fix: add transition to Get Started button hover state
    Prevents jarring snap when hover interrupts drift animation

--- docs/.vitepress/theme/components/ButtonBar.vue
+++ docs/.vitepress/theme/components/ButtonBar.vue
@@ -185,6 +185,7 @@ onUnmounted(() => {
 /* Hover: shift perspective */
 .bb-3d:hover::before {
   transform: translate(0.75rem, -0.75rem);
+  transition: transform 180ms ease, background-color 150ms ease, outline-color 150ms ease;
 }
 
 .bb-3d:hover::after {
